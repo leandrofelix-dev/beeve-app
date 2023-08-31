efb3c435e5383790bc9ad8f5cab6aa957193756c
lehinfo-felix <lehinfo.felix@gmail.com>
2023-08-31 10:52:58 -0300

    correção dos assets

--- app.config.js
+++ app.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  name: 'ImHere',
+  name: 'Beeve',
+  slug: 'beeve',
   version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#000000',
+  },
+  android: {
+    package: 'com.felixdev.beeve',
+  },
   extra: {
+    eas: {
+      projectId: '74e7e37c-7c99-457d-9736-1ea05cf5bee1',
+    },
     apiUrl: process.env.API_URL,
   },
 }
